c9105b7309274c24b5072c1bdf35b3b3992975c2
wangjie <wangjie@wangjiedeMBP.lan>
2022-08-13 16:49:59 +0800

    增加返回的页面

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,21 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class DemoApplication {
 
-	@GetMapping("/")
-	public Map getDemo(){
-		Map<String,String> result = new HashMap<>();
-		result.put("hello", "world");
-		return result;
+	@RequestMapping("/")
+	public String index() {
+		return "index";
 	}
 
 	public static void main(String[] args) {
